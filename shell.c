89470fbd71c1cab14891cc1540775440db14b7dd
Felipe Melo <56006492+felipsdmelo@users.noreply.github.com>
2021-06-08 18:22:24 -0300

    Soluciona jobs para o caso "killed" e muda nomes de funções

--- shell.c
+++ shell.c
@@ -7,27 +7,28 @@
 #include <signal.h>
 #include <setjmp.h>
 
-#define COLOR_GREEN "\033[0;32;32m"
 #define COLOR_RED "\e[0;31m"
 #define COLOR_YELLOW "\e[0;33m"
 #define COLOR_BOLD_YELLOW "\033[1;33m"
 #define COLOR_UNDERLINE_YELLOW "\e[4;33m"
-#define COLOR_BACKGROUND_YELLOW "\e[43m"
-#define COLOR_BLUE "\e[0;34m"
 #define COLOR_BOLD_BLUE "\e[1;34m"
 #define COLOR_CYAN "\033[0;36m"
 #define COLOR_RESET "\x1b[0m"
 #define CLEAR "\e[2J\e[H"
 
-#define SHELL_BUFSIZE 1024
-#define BUFSIZE_TOK 64
-#define TOK_DELIM " \t\r\n\a"
+#define BUFFER_SIZE 2048
+#define BUFFER_SIZE_TOKEN 128
+#define LIMITADORES " \t\r\n\a"
 
+// declaracoes de funcoes built-in
 int cd(char **args);
 int help(char **args);
 int shellExit(char **args);
+int bg(char **args);
 
-char *builtin_str[] = { "cd", "help", "exit", "jobs"};
+int imprimeJob(); // declaracao da funçao imprimeJob
+int (*builtin_func[]) (char **) = {&cd, &help, &shellExit, &imprimeJob, &bg};
+char *builtin_str[] = { "cd", "help", "exit", "jobs", "bg"};
 
 typedef struct jobs {
     int posicao;
@@ -36,50 +37,62 @@ typedef struct jobs {
     pid_t processo;
 } jobs;
 
-static jmp_buf env;
+static jmp_buf buf;
 pid_t pid;
 jobs listaJobs[200];
 int qtdJobs = 0;
+int array[20];
 
-int imprimeJobs(); // declaraçao da funçao imprimeJobs
-int (*builtin_func[]) (char **) = {&cd, &help, &shellExit, &imprimeJobs};
 
-void killJobs(pid_t id)
+void killJob(pid_t id)
 {
     for (int i = 0 ; i < qtdJobs ; i++)
     {
         if (listaJobs[i].processo == id)
         {
             kill(id, SIGKILL);
-            strcpy(listaJobs[i].status, "Done");
+            strcpy(listaJobs[i].status, "Killed  ");
         }
     }
 }
+
+
+void suspendJob(pid_t id)
+{
+    printf("\n");
+    for (int i = 0 ; i < qtdJobs ; i++)
+    {
+        if (listaJobs[i].processo == id)
+        {
+            kill(id, SIGTSTP);
+            strcpy(listaJobs[i].status, "Stopped");
+        }
+    }
+}
+
 
 void sigHandler(int signum)
 {
     if (signum == SIGINT) // Ctrl + C
     {
         if (pid != 0)
-        {
-            killJobs(pid);
-            longjmp(env, 42);
-        }
+            killJob(pid);
     }
     else if (signum == SIGTSTP) // Ctrl + Z
     {
         if (pid != 0)
         {
-            killJobs(pid);
-            longjmp(env, 42);
+            kill(pid, SIGTSTP);
+            longjmp(buf, 42); // restaura o processo para o estado que estava quando setjmp foi chamada
         }
     }
-    fflush(stdout);
 }
+
 
 int qtdBuiltIns() {
     return sizeof(builtin_str) / sizeof(char *);
 }
+
 
 int cd(char **args)
 {
@@ -87,25 +100,45 @@ int cd(char **args)
         fprintf(stderr, COLOR_RED "MyShell: Argumento esperado para \"cd\"\n" COLOR_RESET);
     else
     {
-        if (chdir(args[1]) != 0)
+        if (chdir(args[1]) != 0)    // muda o diretório para o especificado em args[1]
             perror(COLOR_RED "MyShell>" COLOR_RESET);
     }
 
     return 1;
 }
 
+
+int bg(char **args)
+{
+    int percent = '%';
+    if (fork() == 0)
+    {
+        int aux;
+        if (strcmp(args[1], percent) == 0)
+            aux = array[strtol(args + 1, NULL, 10)];
+        else
+            aux = atoi(args);
+
+        kill(aux, SIGCONT);
+    }
+    return 1;
+}
+
+
 int help(char **args)
 {
     printf(COLOR_BOLD_BLUE "help:\n" COLOR_RESET);
-    for (int i = 0 ; i < qtdBuiltIns() ; i++)
+    for (int i = 0 ; i < qtdBuiltIns() ; i++)   // percorre a lista de comandos built-in
         printf(COLOR_CYAN "\t%s\n" COLOR_RESET, builtin_str[i]);
 
     return 1;
 }
 
+
 int shellExit(char **args) {
     exit(0);
 }
+
 
 void registraJob(char **args, pid_t id)
 {
@@ -113,37 +146,38 @@ void registraJob(char **args, pid_t id)
     job.processo = id;
     strcpy(job.nome, args[0]);
     strcpy(job.status, "Running\t");
-    job.posicao = qtdJobs + 1;
+    job.posicao = qtdJobs + 1;  // salva a ordem em que o job foi executado
     listaJobs[qtdJobs] = job;
     qtdJobs++;
 }
 
-int imprimeJobs()
+
+int imprimeJob()
 {
     for (int i = 0 ; i < qtdJobs ; i++)
-        printf(COLOR_CYAN "[%d]  Pid: %d   %s\t%s\n" COLOR_RESET, 
+        printf(COLOR_BOLD_BLUE "[%d]  Pid: %d\t%s\t%s\n" COLOR_RESET, 
             listaJobs[i].posicao, listaJobs[i].processo, listaJobs[i].status, listaJobs[i].nome);
 
     return 1;
 }
 
-int launch(char **args)
+
+int naoBuiltInCommand(char **args)
 {
     int status;
-    pid_t pide;
-    pide = fork();
-    if (pide == 0) // processo filho
+    pid = fork();
+    if (pid == 0) // processo filho
     {
         if (execvp(args[0], args) == -1)
             perror(COLOR_RED "MyShell>" COLOR_RESET);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
     else if (pid < 0)
         perror(COLOR_RED "MyShell>" COLOR_RESET);
     else
     {
         do {
-            registraJob(args, pide);
+            registraJob(args, pid);
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
@@ -151,7 +185,8 @@ int launch(char **args)
     return 1;    
 }
 
-int execute(char **args)
+
+int builtInCommand(char **args)
 {
     if (args[0] == NULL)
         return 1;
@@ -162,125 +197,125 @@ int execute(char **args)
             return (*builtin_func[i])(args);
     }
 
-    return launch(args);
+    return naoBuiltInCommand(args);
 }
+
 
 char *lerLinha()
 {
-    int bufsize = SHELL_BUFSIZE;
+    int tamanhoBuf = BUFFER_SIZE;
     int posicao = 0;
-    char *buffer = (char *) malloc(bufsize * sizeof(char));
-    int c;
+    char *buffer = (char *) malloc(tamanhoBuf * sizeof(char));
+    int ch;
 
     if (!buffer)
     {
         fprintf(stderr, COLOR_RED "MyShell: Erro de alocacao" COLOR_RESET);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     while (1)
     {
-        c = getchar();
-        if (c == EOF) {
-            exit(EXIT_SUCCESS);
+        ch = getchar();
+        if (ch == EOF) {
+            exit(0);
         }
-        else if (c == '\n')
+        else if (ch == '\n')
         {
             buffer[posicao] = '\0';
             return buffer;
         }
         else {
-            buffer[posicao] = c;
+            buffer[posicao] = ch;
         }
         posicao++;
 
-        if (posicao >= bufsize)
+        if (posicao >= tamanhoBuf)
         {
-            bufsize += SHELL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
+            tamanhoBuf += BUFFER_SIZE;
+            buffer = realloc(buffer, tamanhoBuf);
 
             if (!buffer)
             {
                 fprintf(stderr, COLOR_RED "MyShell: Erro de alocacao" COLOR_RESET);
-                exit(EXIT_FAILURE);
+                exit(1);
             }
         }
     }
 }
+
 
 char **splitLinha(char *linha)
 {
-    int bufsize = BUFSIZE_TOK;
+    int tamanhoBuf = BUFFER_SIZE_TOKEN;
     int posicao = 0;
-    char **tokens = (char **) malloc(bufsize * sizeof(char *));
-    char *token, **tokensBackup;
+    char **buf = (char **) malloc(tamanhoBuf * sizeof(char *));
+    char *token, **bufBackup;
 
-    if (!tokens)
+    if (!buf)
     {
         fprintf(stderr, COLOR_RED "MyShell: Erro de alocacao" COLOR_RESET);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    token = strtok(linha, TOK_DELIM);
+    token = strtok(linha, LIMITADORES);
     while (token != NULL)
     {
-        tokens[posicao] = token;
+        buf[posicao] = token;
         posicao++;
 
-        if (posicao >= bufsize)
+        if (posicao >= tamanhoBuf)
         {
-            bufsize += BUFSIZE_TOK;
-            tokensBackup = tokens;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
+            tamanhoBuf += BUFFER_SIZE_TOKEN;
+            bufBackup = buf;
+            buf = realloc(buf, tamanhoBuf * sizeof(char *));
+            if (!buf)
             {
-                free(tokensBackup);
+                free(bufBackup);
                 fprintf(stderr, COLOR_RED "MyShell: Erro de alocacao" COLOR_RESET);
-                exit(EXIT_FAILURE);
+                exit(1);
             }
         }
-        token = strtok(NULL, TOK_DELIM);
+        token = strtok(NULL, LIMITADORES);
     }
 
-    tokens[posicao] = NULL;
-    return tokens;
+    buf[posicao] = NULL;
+    return buf;
 }
 
-void loop()
+
+void inicializa()
 {
     char *linha;
     char **args;
     int status;
-    char cwd[200]; // caminho do diretorio
-    signal(SIGINT, sigHandler);
+    char cwd[256]; // caminho do diretorio corrente
+    signal(SIGINT, sigHandler); // lida com os sinais antes
     signal(SIGTSTP, sigHandler);
 
     do
     {
-        if (setjmp(env) == 42)
+        // ponto de volta do salto
+        if (setjmp(buf) == 42)
         {
-            printf("\n");
+            printf("\nNEYYY NEY NEY ");
             continue;
         }
-        //printf(COLOR_BACKGROUND_YELLOW "MyShell" COLOR_RESET);
-        //printf(COLOR_UNDERLINE_YELLOW "MyShell" COLOR_RESET);
-        printf(COLOR_UNDERLINE_YELLOW "MyShell" COLOR_YELLOW ": %s> " COLOR_RESET, 
+        printf(COLOR_UNDERLINE_YELLOW "MyShell" COLOR_YELLOW ": %s$ " COLOR_RESET, 
             getcwd(cwd, sizeof(cwd)));
         fflush(stdout);
         linha = lerLinha();
         args = splitLinha(linha);
-        // registraJob(args);
-        status = execute(args);
+        status = builtInCommand(args);
 
         free(linha); free(args);
     } while (status);
 }
 
+
 int main()
 {
     //printf(CLEAR);
-    signal(SIGINT, sigHandler);
-    signal(SIGTSTP, sigHandler);
-    loop();
+    inicializa();
     return 0;
 }
